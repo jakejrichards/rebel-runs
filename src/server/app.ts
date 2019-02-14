6c8137445b942d376a29f45d8098b62d3096e0da
jakejrichards <jjrichar@go.olemiss.edu>
2019-02-13 19:29:05 -0600

    Firebase setup - work in progress

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import firebase from "firebase";
 
 const app = express();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRnVWBb0gzc1ZmxOUXWe_k6VqfnayhfCI",
+  authDomain: "rebelruns-f0966.firebaseapp.com",
+  databaseURL: "https://rebelruns-f0966.firebaseio.com",
+  projectId: "rebelruns-f0966",
+  storageBucket: "rebelruns-f0966.appspot.com",
+  messagingSenderId: "302383393254"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
