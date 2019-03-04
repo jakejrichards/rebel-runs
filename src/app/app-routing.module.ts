149b53fe64da22860ae52ef2e6d09c83cafde86c
jakejrichards <jjrichar@go.olemiss.edu>
2019-03-04 11:07:31 -0600

    Refactor: move restaurants code from home page to restaurants page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "", redirectTo: "restaurants", pathMatch: "full" },
   { path: "home", loadChildren: "./pages/home/home.module#HomePageModule" },
   { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
   {
     path: "create-restaurant",
     loadChildren:
       "./pages/create-restaurant/create-restaurant.module#CreateRestaurantPageModule"
+  },
+  {
+    path: "restaurants",
+    loadChildren: "./pages/restaurants/restaurants.module#RestaurantsPageModule"
   }
 ];
 
