098afd5c8a78e31dce6e92b950dcdd6ccc2640d3
jakejrichards <jjrichar@go.olemiss.edu>
2019-03-04 13:15:24 -0600

    Create user account in database on new user authenticated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
   constructor(
     public angularFireAuth: AngularFireAuth,
+    private db: AngularFirestore
   ) {}
 
   user = this.angularFireAuth.user;
 
   login() {
-    this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    this.angularFireAuth.auth
+      .signInWithPopup(new auth.GoogleAuthProvider())
+      .then(result => {
+        if (result.additionalUserInfo.isNewUser) {
+          this.db
+            .collection("accounts")
+            .doc(result.user.uid)
+            .set({
+              id: result.user.uid,
+              name: result.user.displayName,
+              email: result.user.email
+            });
+        }
+      });
   }
 
   logout() {
